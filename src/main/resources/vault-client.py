21ff1296094c2785184bb56fe2c8aa764384951a
Luis Toledo <luis@variacode.com>
2024-03-18 12:53:26 -0300

    optional label for ansible vault script

--- src/main/resources/vault-client.py
+++ src/main/resources/vault-client.py
@@ -6,16 +6,20 @@ import getpass
 
 parser = argparse.ArgumentParser(description='Get a vault password from user keyring')
 
-parser.add_argument('--vault-id', action='store', default='dev',
-                        dest='vault_id',
-                        help='name of the vault secret to get from keyring')
+parser.add_argument('--vault-id', action='store', default='',
+                    dest='vault_id',
+                    help='name of the vault secret to get from keyring')
 
 args = parser.parse_args()
 keyname = args.vault_id
 
-if "VAULT_ID_SECRET" in os.environ:
-    secret=os.environ["VAULT_ID_SECRET"]
-    sys.stdout.write('%s/%s\n' % (keyname,secret))
+secret=os.environ["VAULT_ID_SECRET"]
+
+if secret:
+    if keyname:
+        sys.stdout.write('%s/%s\n' % (keyname,secret))
+    else:
+        sys.stdout.write('%s\n' % (secret))
     sys.exit(0)
 
 if sys.stdin.isatty():
@@ -23,10 +27,12 @@ if sys.stdin.isatty():
 else:
     secret = sys.stdin.readline().rstrip()
 
-if secret is None or secret == '':
-    sys.stderr.write('ERROR: secret is not set\n')
+if secret is None:
+    sys.stderr.write('ERROR: VAULT_ID_SECRET is not set\n')
     sys.exit(1)
 
-sys.stdout.write('%s/%s\n' % (keyname,secret))
+if keyname:
+    sys.stdout.write('%s/%s\n' % (keyname,secret))
+else:
+    sys.stdout.write('%s\n' % (secret))
 sys.exit(0)
-
