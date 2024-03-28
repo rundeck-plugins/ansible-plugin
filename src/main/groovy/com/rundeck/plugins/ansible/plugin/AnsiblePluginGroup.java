8a73d5aac14664f5af6df47d598b38c5b564207b
Luis Toledo <luis@variacode.com>
2024-03-28 20:26:43 -0300

    add plugin group

---
+++ src/main/groovy/com/rundeck/plugins/ansible/plugin/AnsiblePluginGroup.java
@@ -0,0 +1,75 @@
+package com.rundeck.plugins.ansible.plugin;
+
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.core.plugins.configuration.Describable;
+import com.dtolabs.rundeck.core.plugins.configuration.Description;
+import com.dtolabs.rundeck.core.plugins.configuration.PluginAdapterUtility;
+import com.dtolabs.rundeck.plugins.ServiceNameConstants;
+import com.dtolabs.rundeck.plugins.config.PluginGroup;
+import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
+import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
+import com.dtolabs.rundeck.plugins.util.DescriptionBuilder;
+
+
+@Plugin(service = ServiceNameConstants.PluginGroup, name = "Ansible Project Configuration")
+@PluginDescription(title = "Ansible", description = "Plugin basic ansible configurations")
+public class AnsiblePluginGroup implements PluginGroup, Describable {
+
+    public String getAnsibleConfigFilePath() {
+        return ansibleConfigFilePath;
+    }
+
+    public void setAnsibleConfigFilePath(String ansibleConfigFilePath) {
+        this.ansibleConfigFilePath = ansibleConfigFilePath;
+    }
+
+    public String getAnsibleBinariesDirPath() {
+        return ansibleBinariesDirPath;
+    }
+
+    public void setAnsibleBinariesDirPath(String ansibleBinariesDirPath) {
+        this.ansibleBinariesDirPath = ansibleBinariesDirPath;
+    }
+
+    public Boolean getEncryptExtraVars() {
+        return encryptExtraVars;
+    }
+
+    public void setEncryptExtraVars(Boolean encryptExtraVars) {
+        this.encryptExtraVars = encryptExtraVars;
+    }
+
+    @PluginProperty(
+            title = "Ansible config file path",
+            description = "Set ansible config file path."
+    )
+    //@RenderingOptions({
+   //         @RenderingOption(key = StringRenderingConstants.GROUP_NAME, value = "Authentication")
+    //})
+    String ansibleConfigFilePath;
+
+    @PluginProperty(
+            title = "Ansible binaries directory path",
+            description = "Set ansible binaries directory path."
+    )
+    //@RenderingOptions({
+    //         @RenderingOption(key = StringRenderingConstants.GROUP_NAME, value = "Authentication")
+    //})
+    String ansibleBinariesDirPath;
+
+    @PluginProperty(
+            title = "Encrypt Extra Vars.",
+            description = "Encrypt the value of the extra vars keys."
+    )
+    //@RenderingOptions({
+    //         @RenderingOption(key = StringRenderingConstants.GROUP_NAME, value = "Authentication")
+    //})
+    Boolean encryptExtraVars;
+
+    @Override
+    public Description getDescription() {
+        DescriptionBuilder builder = DescriptionBuilder.builder();
+        Description description = PluginAdapterUtility.buildDescription(this, builder);
+        return builder.build();
+    }
+}
