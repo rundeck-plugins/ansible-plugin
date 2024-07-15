6fa13c5f7f0a2a90edfb0bf274cb3c8b4c409bff
Luis Toledo <luis@variacode.com>
2024-07-15 18:13:24 -0400

    add test case configuring ansible-inventory without explicit inventory list file

--- functional-test/src/test/groovy/functional/InventoryListSpec.groovy
+++ functional-test/src/test/groovy/functional/InventoryListSpec.groovy
@@ -4,7 +4,7 @@ import functional.base.BaseTestConfiguration
 import org.testcontainers.spock.Testcontainers
 
 @Testcontainers
-class LargeInventorySpec  extends BaseTestConfiguration {
+class InventoryListSpec extends BaseTestConfiguration {
 
     static String PROJ_NAME = 'ansible-large-inventory'
 
@@ -16,11 +16,11 @@ class LargeInventorySpec  extends BaseTestConfiguration {
     def "test large inventory"(){
         when:
 
-        def result = client.apiCall {api-> api.listNodes(PROJ_NAME,".*")}
+        def result = client.apiCall {api-> api.listNodes(PROJ_NAME,"tags:test")}
 
         then:
         result!=null
-        result.size()==8001
+        result.size()==8000
         result.get("Node-0")!=null
         result.get("Node-0").getAttributes().get("ansible_host") == "ssh-node"
         result.get("Node-0").getAttributes().get("ansible_ssh_user") == "rundeck"
@@ -29,5 +29,19 @@ class LargeInventorySpec  extends BaseTestConfiguration {
         result.get("Node-7999").getAttributes().get("ansible_host") == "ssh-node"
         result.get("Node-7999").getAttributes().get("ansible_ssh_user") == "rundeck"
         result.get("Node-7999").getAttributes().get("some-var") == "1234"
+    }
+
+    def "test empty inventory path"(){
+        when:
+
+        def result = client.apiCall {api-> api.listNodes(PROJ_NAME,"tags:fake")}
+
+        then:
+        result!=null
+        result.size()==35
+        result.get("node1")!=null
+        result.get("node1").getAttributes().get("ansible_host") == "node1"
+        result.get("node1").getAttributes().get("ansible_user") == "agent"
+        result.get("node1").getAttributes().get("ansible_port") == "22"
     }
 }
